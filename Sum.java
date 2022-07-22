96f0be659dfd85863ec5a67b3e71c892626d8fd1
Nirmal <nirmalkr805@gmail.com>
2022-07-22 13:09:59 +0530

    Sum

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        // Sum of two numbers
+        Scanner in = new Scanner(System.in);
+        System.out.print("First number: ");
+        int num1 = in.nextInt();
+        System.out.print("Second number: ");
+        int num2 = in.nextInt();
+        int sum=num1+num2;
+        System.out.println("Sum of "+num1+ " and "+ num2+" = "+sum);
+    }
+}
