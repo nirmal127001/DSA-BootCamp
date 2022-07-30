98737ceeb2d2248161ef026ee2e6f847ea6a7789
Nirmal <nirmalkr805@gmail.com>
2022-07-30 18:50:30 +0530

    num of days

---
+++ NumOfDaya.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Kunal is allowed to go out with his friends only on the even days of a given month. 
+//Write a program to count the number of days he can go out in the month of August.
+public class NumOfDaya {
+    public static void main(String[] args) {
+        int count=0;
+        Scanner in = new Scanner(System.in);
+        for (int i = 1; i<31; i++){
+            if(i%2==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
